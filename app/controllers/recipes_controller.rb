c176402ffcf1d77216ba5a5057cc154ac387b321
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-05 22:48:38 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "--First Ingredient--")
+    @recipe.ingredients.build(name: "--Second Ingredient--")
   end
 
   def create
